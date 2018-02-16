ea88145a5f2ccc5559b12da6c89b4696ab440830
Flaviano Flauber <flauberjp@gmail.com>
2018-02-16 13:30:24 -0300

    Used a service to provide the list of people. Ready for the next step(http://goo.gl/BFwziy)

--- star-war-app/src/app/app.module.ts
+++ star-war-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PeopleListComponent } from './people-list/people-list.component';
+import { PeopleService } from './people.service';
 
 
 @NgModule({
@@ -14,7 +15,10 @@ import { PeopleListComponent } from './people-list/people-list.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    PeopleService 
+  //providers: [ {provider:'IPeopleService', useClass: PeopleService} ]
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- star-war-app/src/app/people-list/people-list.component.ts
+++ star-war-app/src/app/people-list/people-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Person } from '../person';
+import { PeopleService } from '../people.service';
 
 @Component({
   selector: 'app-people-list',
@@ -14,19 +15,16 @@ import { Person } from '../person';
 })
 
 export class PeopleListComponent implements OnInit {
-  people: Person[] = [
-    {
-      name: 'Luke Skywalker', 
-      height: 177,
-      weight: 70
-    },
-    {name: 'Darth Vader', height: 200, weight: 100},
-    {name: 'Han Solo', height: 185, weight: 85}
-  ];
+  people: Person[] = [];
 
-  constructor() { }
+  constructor(private peopleService: PeopleService) { 
+  //constructor(@Inject("IPeopleService")private _peopleService: IPeopleService) { 
+
+  }
 
   ngOnInit() {
+      //this.people = _peopleService.getAll();  
+      this.people = peopleService.getAll();
   }
 
 }
