5817389c9e94a6e1157b1391e0f91a73e1a8b3f4
Flaviano Marinho <flauberjp@gmail.com>
2018-02-25 16:19:43 -0300

    Practice to test the .NET portatility finished

---
+++ TestingDotNetPortability/SelfContainedApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SelfContainedApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
