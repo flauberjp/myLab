fcf7fda3978c50dfc0940bff044c68ffea431f6b
Flaviano Marinho <flauberjp@gmail.com>
2018-03-11 22:34:03 -0300

    IdentityServer4: Playing with IdentityServer4, and Web Applicaiton Client

---
+++ MyIdentityServer4/MyIdentityServer4/Config.cs
@@ -0,0 +1,96 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using static IdentityModel.OidcConstants;
+
+namespace MyIdentityServer4
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client> {
+                new Client {
+                    ClientId = "oauthClient",
+                    ClientName = "Example Client Credentials Client Application",
+                    AllowedGrantTypes = new List<string> { OidcConstants.GrantTypes.ClientCredentials },
+                    ClientSecrets = new List<Secret> {
+                        new Secret("superSecretPassword".Sha256())},
+                    AllowedScopes = new List<string> {"customAPI.read"}
+                },
+                new Client {
+                    ClientId = "openIdConnectClient",
+                    ClientName = "Example Implicit Client Application",
+                    ClientUri = "http://localhost:54486",
+                    AllowedGrantTypes = new List<string> { OidcConstants.GrantTypes.Implicit },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "role",
+                        "customAPI.write"
+                    },
+                    AllowedCorsOrigins = { "http://localhost:54486" },
+
+                    // where to redirect to after login
+                    RedirectUris = new List<string> {"https://localhost:44350/signin-oidc"},
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = new List<string> { "https://localhost:44350/signout-callback-oidc" }
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource> {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource {
+                    Name = "role",
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource> {
+                new ApiResource {
+                    Name = "customAPI",
+                    DisplayName = "Custom API",
+                    Description = "Custom API Access",
+                    UserClaims = new List<string> {"role"},
+                    ApiSecrets = new List<Secret> {new Secret("scopeSecret".Sha256())},
+                    Scopes = new List<Scope> {
+                        new Scope("customAPI.read"),
+                        new Scope("customAPI.write")
+                    }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser> {
+                new TestUser {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "scott",
+                    Password = "password",
+                    Claims = new List<Claim> {
+                        new Claim(JwtClaimTypes.Email, "scott@scottbrady91.com"),
+                        new Claim(JwtClaimTypes.Role, "admin")
+                    }
+                }
+            };
+        }
+    }    
+}
