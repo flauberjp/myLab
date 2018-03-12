b7859597aae9e04660522dd691b6928b95d89802
Flaviano Flauber <flauberjp@gmail.com>
2018-03-12 08:29:39 -0300

    Continuing to work on angular-oidcclientjs-example projct

--- angular-oidcclientjs-example/src/app/app-routing.module.ts
+++ angular-oidcclientjs-example/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProtectedComponent } from './protected/protected.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: []
+  },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [AuthGuardService]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-oidcclientjs-example/src/app/app.module.ts
+++ angular-oidcclientjs-example/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ProtectedComponent } from './protected/protected.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProtectedComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthGuardService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
