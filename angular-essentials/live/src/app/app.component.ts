6e925ae1aa13de6202cd95ece635f80fde88e31b
Flaviano Marinho <flauberjp@gmail.com>
2018-04-01 10:56:27 -0300

    Continuing the practices of the course

--- angular-essentials/live/src/app/app.component.ts
+++ angular-essentials/live/src/app/app.component.ts
@@ -6,10 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  number = 0;
   title = 'app';
   rootName = 'Maximilian';
 
   onNameChanged(newName) {
     this.rootName = newName;
+  }
+
+  increase() {
+    this.number = this.number + 1;
   }
 }
