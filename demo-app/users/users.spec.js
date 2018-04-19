d8c9532c62d2626a1065d07d59f2c6b7ca83e0e4
Flaviano Marinho <flauberjp@gmail.com>
2018-04-19 18:07:48 -0300

    New practice: 'Testes unitários em aplicações AngularJs com Jasmine e Karma'

---
+++ demo-app/users/users.spec.js
@@ -0,0 +1,6 @@
+describe('Users factory', function() {
+    it('O resultado deve ser 4', function() {
+        // Estamos esperando que o resultado seja 4
+        expect(2 + 2).toEqual(4);
+    })
+})
