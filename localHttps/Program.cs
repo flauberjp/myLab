8bcf0462dc2556a8da9b604586f018242cf4e8b4
Flaviano Flauber <flauberjp@gmail.com>
2018-05-29 14:58:07 -0300

    New Practice: How to run a self-signed http server accessible externally(using machine IP)

---
+++ localHttps/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace localHttps
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddEnvironmentVariables()
+               .AddJsonFile("certificate.json", optional: true, reloadOnChange: true)
+               .AddJsonFile($"certificate.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
+               .Build();
+
+            var certificateSettings = config.GetSection("certificateSettings");
+            string certificateFileName = certificateSettings.GetValue<string>("filename");
+            string certificatePassword = certificateSettings.GetValue<string>("password");
+
+            var certificate = new X509Certificate2(certificateFileName, certificatePassword);
+
+            var host = new WebHostBuilder()
+               .UseKestrel(
+                   options =>
+                   {
+                       options.AddServerHeader = false;
+                       options.Listen(IPAddress.Parse(GetLocalIPAddress()), 44321, listenOptions =>
+                       {
+                           listenOptions.UseHttps(certificate);
+                       });
+                   }
+               )
+               .UseConfiguration(config)
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .UseStartup<Startup>()
+               //.UseUrls("https://localhost:44321")
+               .Build();
+
+            host.Run();
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+}
---
+++ localHttps/README.md
@@ -0,0 +1,7 @@
+Ref.: 
+[Develop Locally with HTTPS, Self-Signed Certificates and ASP.NET Core](https://www.humankode.com/asp-net-core/develop-locally-with-https-self-signed-certificates-and-asp-net-core)
+[Create a domain signed SSL certificate using PowerShell](https://community.spiceworks.com/topic/1991368-create-a-domain-signed-ssl-certificate-using-powershell)
+[Self-signed certificate generator (PowerShell)](https://gallery.technet.microsoft.com/scriptcenter/Self-signed-certificate-5920a7c6)
+[How to call a function in another PowerShell script when executing PowerShell script using 'Run With PowerShell'](https://stackoverflow.com/questions/8501225/how-to-call-a-function-in-another-powershell-script-when-executing-powershell-sc)
+[When given .crt and .key files, make a .pfx file](https://www.sherweb.com/blog/when-given-crt-and-key-files-make-a-pfx-file/)
+[Get local IP address](https://stackoverflow.com/questions/6803073/get-local-ip-address)
