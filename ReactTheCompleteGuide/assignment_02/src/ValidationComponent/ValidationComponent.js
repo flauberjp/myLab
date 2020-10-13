4f622f38745d60af7a8094968527dccbb5e635e2
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2020-10-13 11:39:37 -0300

    assignment_02: 3. Inside the ValidationComponent...

--- ReactTheCompleteGuide/assignment_02/src/ValidationComponent/ValidationComponent.js
+++ ReactTheCompleteGuide/assignment_02/src/ValidationComponent/ValidationComponent.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const validationComponent = (props) => {
+  let output = "Text too short";
+
+  if (props.textLength >= 5) {
+    output = "Text long enough";
+  }
   return (
     <div>
       <p>ValidationComponent</p>
-      <p>{props.textLength}</p>
+      <p>{output}</p>
     </div>
   );
 };
