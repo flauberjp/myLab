97a2115a26c0d6996773205c735c555eccfd7804
Flaviano Flauber <fmarinho@tce.pb.gov.br>
2020-10-13 11:49:40 -0300

    assignment_02: 4. Create another component (=> CharComponent)...

--- ReactTheCompleteGuide/assignment_02/src/App.js
+++ ReactTheCompleteGuide/assignment_02/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ValidationComponent from "./ValidationComponent/ValidationComponent";
 import "./App.css";
+import CharComponent from "./CharComponent/CharComponent";
 
 function App() {
   const [textLength, setTextLength] = useState(0);
@@ -14,6 +15,7 @@ function App() {
       <input onChange={onChangeHandler} type="text" />
       <p>Length of the entered text: {textLength}</p>
       <ValidationComponent textLength={textLength} />
+      <CharComponent></CharComponent>
     </div>
   );
 }
